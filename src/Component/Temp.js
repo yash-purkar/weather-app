3687e698fed967e136377b2a152532cb7fa26661
yashpurkar <yashpurkar7079@gmail.com>
2022-11-30 12:13:43 +0530

    data showed

--- src/Component/Temp.js
+++ src/Component/Temp.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import todo from './to_do.svg'
+import WeatherCard from './WeatherCard';
 
 
 const Temp = () => {
   const [searchValue, setSearchValue] = useState("pune");
+  const [tempInfo, setTempInfo] = useState("");
 
   const getWeatherInfo = async () => {
 
     try {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=2d9f020388f73c365b0cc6457b04233e`;
+
 
 
       const res = await fetch(url);
@@ -18,9 +20,22 @@ const Temp = () => {
       const { temp, humidity, pressure } = data.main;
       const { main: weatherMood } = data.weather[0];
       const { name: cityName } = data;
+      const { speed } = data.wind;
       const { country, sunset } = data.sys;
       console.log(temp, humidity, pressure, weatherMood, cityName, country, sunset);
 
+      const weatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        speed,
+        weatherMood,
+        cityName,
+        country,
+        sunset
+      };
+
+      setTempInfo(weatherInfo);
 
     } catch (error) {
       alert("NotFound")
@@ -48,86 +63,9 @@ const Temp = () => {
         </div>
       </div>
 
-      {/* temp card */}
-      <article className='widget'>
-        <div className="weatherIcon">
-          {/* <i className={"wi wi-day-sunny"}></i> */}
-          {/* Use icon here instead of img */}
-          <img src={todo} alt="" style={{ width: "150px" }} />
-        </div>
 
-        <div className="weatherInfo">
-          <div className="temperature">
-            <span>25 &deg;</span>
-          </div>
+      <WeatherCard tempInfo={tempInfo} />
 
-          <div className="description">
-
-            <div className="weatherCondition">
-              Sunny
-            </div>
-
-            <div className="place">Ahmednagar, India</div>
-          </div>
-        </div>
-
-        <div className="date">
-          {new Date().toLocaleString()}
-        </div>
-
-        {/* Four column section */}
-        <div className="extra_temp">
-          <div className="temp_info">
-            <div className="two_side_section">
-              <p>
-                {/* <i></i> */}
-                <img src={todo} alt="" style={{ width: "75px" }} />
-              </p>
-              <p className="extra_info_left">
-                19:19 PM <br />
-                Sunset
-              </p>
-            </div>
-          </div>
-
-
-          <div className="two_side_section">
-            <p>
-              {/* <i></i> */}
-              <img src={todo} alt="" style={{ width: "75px" }} />
-            </p>
-            <p className="extra_info_left">
-              19:19 PM <br />
-              Humidity
-            </p>
-          </div>
-        </div>
-
-        <div className="weather_extra_info">
-          <div className="two_side_section">
-            <p>
-              {/* <i></i> */}
-              <img src={todo} alt="" style={{ width: "75px" }} />
-            </p>
-            <p className="extra_info_left">
-              19:19 PM <br />
-              Pressure
-            </p>
-          </div>
-
-          <div className="two_side_section">
-            <p>
-              {/* <i></i> */}
-              <img src={todo} alt="" style={{ width: "75px" }} />
-            </p>
-            <p className="extra_info_left">
-              19:19 PM <br />
-              Speed
-            </p>
-          </div>
-        </div>
-
-      </article>
     </>
   )
 }
